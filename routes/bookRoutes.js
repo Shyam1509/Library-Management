e9d591b950ae5b484397aa81a737d59b706bd409
ghanshyam-rd <ghanshyam.mahajan@rejigdigital.com>
2025-08-25 14:53:06 +0530

    Reorganized project with backend folder

--- routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addBook, searchBook } = require("../controllers/bookController");
-
-const bookRoutes = express.Router();
-
-bookRoutes.post("/add-book", addBook);
-bookRoutes.get("/search", searchBook);
-
-module.exports = bookRoutes;
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { addUser } = require("../controllers/userController");
-
-const userRoutes = express.Router();
-
-userRoutes.post("/add-user", addUser);
-
-module.exports = userRoutes;
